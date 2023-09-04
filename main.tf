1a4fc9858994425b1f6491e65b80740a235896a1
Livia Vilcu <livia.vilcu@avaloq.com>
2023-09-04 15:42:44 +0200

    Initial commit

--- main.tf
+++ main.tf
@@ -20,6 +20,26 @@ resource "oci_container_instances_container_instance" "this" {
   }
 }
 
+resource "oci_core_internet_gateway" "demo_igw" {
+
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.test_vcn.id
+  display_name   = "demo-vcn - Internet gateway"
+  enabled        = true
+}
+
+resource "oci_core_route_table" "demo_igw_rt" {
+
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.test_vcn.id
+  display_name   = "demo vcn - Internet gateway route table"
+  route_rules {
+
+    network_entity_id = oci_core_internet_gateway.demo_igw.id
+    destination       = "0.0.0.0/0"
+  }
+}
+
 resource "oci_core_vcn" "test_vcn" {
   cidr_blocks    = ["10.0.0.0/16"]
   compartment_id = var.compartment_ocid
@@ -33,3 +53,22 @@ resource "oci_core_subnet" "test_subnet" {
   dns_label      = "TestSubnet"
 }
 
+resource "oci_core_internet_gateway" "test_igw" {
+
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.test_vcn.id
+  display_name   = "test-vcn - Internet gateway"
+  enabled        = true
+}
+
+resource "oci_core_route_table" "test_igw_rt" {
+
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.test_vcn.id
+  display_name   = "test vcn - Internet gateway route table"
+  route_rules {
+
+    network_entity_id = oci_core_internet_gateway.test_igw.id
+    destination       = "0.0.0.0/0"
+  }
+}
