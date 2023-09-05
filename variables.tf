e1b72004b4071e2a48b01d705ade3566d614e9f7
Livia Vilcu <livia.vilcu@avaloq.com>
2023-09-05 07:07:17 +0200

    Add env variables

--- variables.tf
+++ variables.tf
@@ -4,4 +4,9 @@ variable "container_name" {}
 variable "container_image_url" {}
 variable "shape_config_ocpus" {}
 variable "region" {}
-variable "env_variables"{}
+variable "env_variables"{
+  type = map(any)
+  default = {
+    environment = "dev"
+  }
+}
